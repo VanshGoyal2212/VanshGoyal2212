81686ec536efcd9578780d55567feeba7ca442d0
VanshGoyal2212 <vansh.25b01010052@abes.ac.in>
2026-07-23 22:44:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+new Typed("#typing",{
+strings:[
+"C++ Developer",
+"Java Programmer",
+"DSA Enthusiast",
+"Cloud Computing Learner",
+"Open Source Learner"
+],
+typeSpeed:70,
+backSpeed:40,
+loop:true
+});
+
+particlesJS("particles-js",{
+particles:{
+number:{value:80},
+color:{value:"#58a6ff"},
+shape:{type:"circle"},
+opacity:{value:0.5},
+size:{value:3},
+move:{enable:true,speed:2}
+}
+});
